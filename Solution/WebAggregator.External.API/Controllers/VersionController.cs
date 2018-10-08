c984a695c76fa25da1702335fa68745ef59fe843
Artur Usmanov <arnystreamer@gmail.com>
2018-10-09 02:08:12 +0300

    #1 adding API project

---
+++ Solution/WebAggregator.External.API/Controllers/VersionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAggregator.External.API.Controllers
+{
+	[Route("api/Version")]
+	public class VersionController : Controller
+	{
+		[HttpGet]
+		public string Get()
+		{
+			return "1.0.0.0";
+		}
+	}
+}
