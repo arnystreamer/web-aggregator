e9cf491af504e71ae6fd748c1abaa9ffaac4482f
Artur Usmanov <arnystreamer@gmail.com>
2024-12-09 22:21:08 +0100

    new web API project

---
+++ backend/Jimx.WebAggregator.API/Options/GeneralOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Jimx.WebAggregator.API.Options
+{
+	public class GeneralOptions
+	{
+		public const string OptionName = "General";
+
+		public AuthGeneralOptions Auth { get; set; } = new AuthGeneralOptions();
+		public string BaseUrl { get; set; } = string.Empty;
+		public string FrontendUrl { get; set; } = string.Empty;
+	}
+}
---
+++ backend/Jimx.WebAggregator.API/Services/KeysProvider.cs
@@ -0,0 +1,24 @@
+﻿using Jimx.WebAggregator.API.Options;
+using Microsoft.Extensions.Options;
+using static System.Linq.Enumerable;
+
+namespace Jimx.WebAggregator.API.Services
+{
+	public class KeysProvider
+	{
+		public KeysProvider(IOptions<GeneralOptions> options)
+		{
+			Random rnd;
+			string GetRandom()
+			{
+				rnd = new Random();
+				return Range(1, 4).Select(x => rnd.Next().ToString("X8")).Aggregate((a, b) => a + b);
+			}
+
+			CredentialsSigningKey = options.Value.Auth?.CredentialsSigningKey ?? GetRandom();
+		}
+
+		public string CredentialsSigningKey { get; init; }
+		public string IssuerSigningKey { get; init; }
+	}
+}
