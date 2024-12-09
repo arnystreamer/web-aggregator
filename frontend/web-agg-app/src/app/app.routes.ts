10bc450916f8f1a7b2acd24e6fd8e3d888823e01
Artur Usmanov <arnystreamer@gmail.com>
2024-12-09 21:59:00 +0100

    new Angular 19 project

---
+++ frontend/web-agg-app/src/app/app.routes.ts
@@ -0,0 +1,36 @@
+import { Routes } from '@angular/router';
+import { StartPageComponent } from './start-page/start-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { LayoutComponent } from './layout/layout.component';
+import { AdmCostOfLivingPageComponent } from './admin-page/adm-cost-of-living-page/adm-cost-of-living-page.component';
+import { CostOfLivingPageComponent } from './data-analysis/cost-of-living-page/cost-of-living-page.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: StartPageComponent
+      },
+      {
+        path: 'admin',
+        children: [
+          {
+            path: '',
+            component: AdminPageComponent
+          },
+          {
+            path: 'cost-of-living',
+            component: AdmCostOfLivingPageComponent
+          }
+        ]
+      },
+      {
+        path: 'cost-of-living',
+        component: CostOfLivingPageComponent
+      }
+    ]
+  }
+];
---
+++ frontend/web-agg-app/src/app/start-page/start-page.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+
+
+@Component({
+  selector: 'wa-start-page',
+  imports: [
+    RouterModule,
+    MatCardModule,
+    MatChipsModule
+  ],
+  templateUrl: './start-page.component.html',
+  styleUrl: './start-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StartPageComponent {
+
+}
